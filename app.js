86a2f47e7e916644f72043ef1bf40dd7861f946b
Charles Ivia <iviacharles@gmail.com>
2020-09-15 13:47:34 +0300

    Setting cookies using the cookie parser library

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv/config");
-
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
@@ -32,7 +33,13 @@ app.use(authRoutes);
 //cookies
 
 app.get("/set-cookies", (req, res) => {
-  res.setHeader("Set-Cookie", "newUser=true");
+  // res.setHeader("Set-Cookie", "newUser=true");
+  res.cookie("newUser", false);
+  res.cookie("isEmployee", true, {
+    maxAge: 1000 * 60 * 60 * 24,
+    // secure: true,
+    httpOnly: true,
+  });
   res.send("You got the cookie!");
 });
 
