a048071e2be083ca49a54118330978ff76e5cb5e
Charles Ivia <iviacharles@gmail.com>
2020-09-16 14:25:51 +0300

    Setting jwt as a cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -53,7 +53,9 @@ module.exports.signup_post = async (req, res) => {
       email,
       password,
     });
-    res.status(200).json(user);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(200).json({ user: user._id });
   } catch (err) {
     const errors = handleErrors(err);
     res.status(400).json({ errors });
